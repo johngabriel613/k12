51f3476ec5f90387b12ffa093f9ac849b68c699b
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2023-04-27 21:44:58 +0800

    update testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,8 @@ const Navbar = () => {
         </div>
         <FontAwesomeIcon className='block md:hidden cursor-pointer' icon={faBars} onClick={handleNav}/>
         <ul className={`${!nav ? 'invisible opacity-0' : 'visible opacity-100'} transition-all duration-300 ease-in-out text-sm flex flex-col items-center gap-2 b-shadow bg-[#1c1d21] py-2 px-4 rounded-md text-center absolute right-0 top-16 md:static md:bg-transparent md:flex-row md:gap-6 md:shadow-none md:visible md:opacity-100`}>
-          <li><Link to='/' onClick={handleNav}>home</Link></li>
+          <li><Link to='/k12/' onClick={handleNav}>home</Link></li>
           <li><Link to='about' onClick={handleNav}>about</Link></li>
-          <li><Link to='rate-us' onClick={handleNav}>rate us</Link></li>
           <li className='flex'><Link className='call-to-action' to='test' onClick={handleNav}>test now</Link></li>
         </ul>
       </nav>
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 const Result = ({result, event}) => {
   return (
@@ -6,9 +7,12 @@ const Result = ({result, event}) => {
       <div className='max-w-[450px] mx-auto text-center grid gap-4 place-items-center'>
       <h1 className='text-3xl font-bold'>HOORAY!</h1>
       <p className='text-sm'>Based on your answers, your recommended senior high school strand is:</p>
-      <h2 className='text-6xl font-black text-primary'>{result}</h2>
+      <div className='w-full relative'>
+        <Player src='https://assets2.lottiefiles.com/packages/lf20_obhph3sh.json' className='absolute w-full max-w-[400px] top-[-150px]' autoplay />
+        <h2 className='text-6xl font-black text-primary animate__animated animate__fadeIn animate__delay-1s'>{result}</h2>
+      </div>
       <p className='text-sm'>Congratulations and best of luck in your senior high school journey!</p>
-      <button className='call-to-action w-fit' onClick={event}>Test again</button>
+      <button className='call-to-action w-fit z-10' onClick={event}>Test again</button>
     </div>
     </div>
   )
