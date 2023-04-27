5fcd4d32388de9046edbf636e423b6705b75d4bb
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2023-04-27 21:18:45 +0800

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/*' element={<App/>}/>
+        <Route path='/k12/*' element={<App/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
